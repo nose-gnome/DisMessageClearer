afa8ecf522eae564273f7a7a90c00d11567c926a
nose-gnome <71675376+nose-gnome@users.noreply.github.com>
2022-04-28 13:22:44 +0100

    Initial commit

---
+++ __main__.py
@@ -0,0 +1,27 @@
+from os import environ
+from discord import Client
+from datetime import datetime, timedelta, timezone
+from  asyncio import sleep as asleep
+
+# Press the green button in the gutter to run the script.
+client = Client()
+
+async def auto_clear():
+    channel = client.get_channel(int(environ['CHANNEL']))
+
+    while True:
+        msg = []
+        time = datetime.utcnow() - timedelta(hours=1)
+        await channel.purge(before=time)
+        await asleep(10)
+
+@client.event
+async def on_ready():
+    print("Logged in")
+    client.loop.create_task(auto_clear())
+
+
+if __name__ == '__main__':
+    client.run(environ['TOKEN'])
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
